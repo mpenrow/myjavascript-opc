7fbe4565dd78c3e15c491ed5a28cd71735c7172e
Michael Penrow <106648+mpenrow@users.noreply.github.com>
2021-11-04 13:02:01 -0400

    Finished creating property getters and setters section of module 3.

--- demo.js
+++ demo.js
@@ -1,10 +1,28 @@
 "use strict";
 (function () {
   let person = {
-    firstName: "Jim",
-    lastName: "Cooper",
+    name: {
+      first: "Jim",
+      last: "Cooper",
+    },
     age: 29,
   };
 
-  display(Object.getOwnPropertyDescriptor(person, "firstName"));
+  Object.defineProperty(person, "fullName", {
+    get: function () {
+      return this.name.first + " " + this.name.last;
+    },
+    set: function (value) {
+      var nameParts = value.split(" ");
+      this.name.first = nameParts[0];
+      this.name.last = nameParts[1];
+    },
+  });
+
+  person.fullName = "Fred Jones";
+
+  display(person.fullName);
+
+  display(person.name.first);
+  display(person.name.last);
 })();
