3f23e2d98a429e0453fd689e01fbcc9af051b63c
Michael Penrow <106648+mpenrow@users.noreply.github.com>
2021-11-06 13:27:45 -0400

    Finished instance vs prototype properties section of module 4.

--- demo.js
+++ demo.js
@@ -7,14 +7,10 @@
 
   Person.prototype.age = 29;
 
-  display(Person.prototype);
-
   let jim = new Person("Jim", "Cooper");
   let sophia = new Person("Sofia", "Cooper");
-  sophia.__proto__.age = 19;
 
-  display(jim.__proto__);
-  display(sophia.__proto__);
-
-  display(Person.prototype === jim.__proto__);
+  jim.age = 18;
+  display(jim.hasOwnProperty("age"));
+  display(jim.age);
 })();
